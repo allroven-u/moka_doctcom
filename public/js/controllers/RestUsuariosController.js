1b7b96c88d93824fe3ea2bd01b079037bb64be2f
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2022-07-31 15:45:20 -0600

    ocultar por usuarios

---
+++ public/js/controllers/RestUsuariosController.js
@@ -0,0 +1,20 @@
+'use strict'
+// Roles #1 Admin , #2 Cliente , #3 Veterinario , #4 Secretaria.
+
+let userLogin = 206790172;
+let rolUser = 1;
+
+
+
+if (rolUser == 2){//ocultar opciones Cliente
+
+}
+else if(rolUser == 3){//ocultar opciones Veterinario
+
+}
+else if(rolUser == 4){//ocultar opciones Secretaria
+    
+}
+else{
+
+}
