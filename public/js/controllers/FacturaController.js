6a272b2e1fd37984e9dccf8c5ad3106970de5968
Dns2690 <dsalazarc@ucenfotec.ac.cr>
2022-08-04 16:42:24 -0600

    validacion de tarjetas

--- public/js/controllers/FacturaController.js
+++ public/js/controllers/FacturaController.js
@@ -27,3 +27,173 @@ function ShowModalPagoFunct() {
 showPagar.addEventListener('click', function() {
     ShowModalPagoFunct();
 });
+
+
+//////////////////////////TARJETAS///////////////////////
+
+
+let inputNombreTitular = document.getElementById("txtTitular");
+let inputNumTarjeta = document.getElementById("numTarjeta");
+let inputNumCVV = document.getElementById("txtCvv");
+let inputMesVenc = document.getElementById("mes_vencimiento");
+let inputYearVenc = document.getElementById("year_vencimiento");
+
+
+
+let tipoVisa = document.getElementById("imgVisa");
+let tipoMaster = document.getElementById("imgMaster");
+let tipoAmex = document.getElementById("imgAmex");
+
+let actualDate = new Date();
+let actualYear = actualDate.getFullYear();
+let actualMonth = actualDate.getMonth();
+
+
+
+function RealizarPago(){
+    if(ValidarDatostarjeta() == true){
+        ConfirmarDatosT();
+    }
+}
+
+function ValidarDatostarjeta(){
+    let sNombreTitular = inputNombreTitular.value;
+    let sNumTarjeta = inputNumTarjeta.value;
+    let sNumCVV = inputNumCVV.value;
+    let nMesV = inputMesVenc.value;
+    let nYearV = inputYearVenc.value;
+
+    if (sNombreTitular == null || sNombreTitular == undefined || sNombreTitular == "") {
+        inputNombreTitular.classList.add("TError");
+        MostrarErrorT();
+        return false;
+    } else {
+        inputNombreTitular.classList.remove("TError");
+    }
+    if (sNumTarjeta == null || sNumTarjeta == undefined || sNumTarjeta == "" ) {
+        inputNumTarjeta.classList.add("TError");
+        MostrarErrorT();
+        return false;
+    }
+    if(ValidarTipoTarjeta() == true){
+            inputNumTarjeta.classList.remove("TError");
+    }else{
+        return false;
+    }
+    if (sNumCVV == null || sNumCVV == undefined || sNumCVV == "") {
+        inputNumCVV.classList.add("TError");
+        MostrarErrorT();
+        return false;
+    } else {
+        inputNumCVV.classList.remove("TError");
+    }
+    if (nMesV == null || nMesV == undefined || nMesV == "") {
+        inputMesVenc.classList.add("TError");
+        MostrarErrorT();
+        return false;
+    } else {
+        inputMesVenc.classList.remove("TError");
+    }
+    if (nYearV == null || nYearV == undefined || nYearV == "") {
+        inputYearVenc.classList.add("TError");
+        MostrarErrorT();
+        return false;
+    } else {
+        inputYearVenc.classList.remove("TError");
+    }
+    if (Number(nYearV) == actualYear ) {
+        if(Number(nMesV)<=actualMonth){
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Tarjeta Vencida',
+                
+            });
+            inputMesVenc.classList.add("TError");
+            inputYearVenc.classList.add("TError");
+            return false;
+
+        }else if(Number(nYearV) < actualYear){
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Tarjeta Vencida',
+            });
+            inputMesVenc.classList.add("TError");
+            inputYearVenc.classList.add("TError");
+            return false;
+        }   
+    }else{
+        inputMesVenc.classList.remove("TError");
+        inputYearVenc.classList.remove("TError");
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ValidarTipoTarjeta(){
+    let nTarjeta = inputNumTarjeta.value;
+    ///////visa////////
+    if(nTarjeta.match(/^4\d{3}-?\d{4}-?\d{4}-?\d{4}$/)){
+        tipoVisa.classList.remove("hidden");
+        tipoMaster.classList.add("hidden");
+        tipoAmex.classList.add("hidden");
+        return true;
+    ///////mastercard/////////////    
+    }else if(nTarjeta.match(/^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/)){
+        tipoVisa.classList.add("hidden");
+        tipoMaster.classList.remove("hidden");
+        tipoAmex.classList.add("hidden");
+        return true;
+        ///////amex/////////////
+    }else if(nTarjeta.match(/^3[4-7]\d{2}-?\d{6}-?\d{5}$/)){
+        tipoVisa.classList.add("hidden");
+        tipoMaster.classList.add("hidden");
+        tipoAmex.classList.remove("hidden");
+        return true;
+    }
+    else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'numero de tarjeta invalido',
+        });
+        tipoVisa.classList.add("hidden");
+        tipoMaster.classList.add("hidden");
+        tipoAmex.classList.add("hidden");
+        return false;
+    }
+
+}
+
+
+function MostrarErrorT() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Dato requerido',
+    })
+}
+
+function ConfirmarDatosT() {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Pago Realizado',
+        showConfirmButton: false,
+        timer: 1500
+    })
+}
+
+
