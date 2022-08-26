3c02c329e3eff7ade5f5bb1f2cf4d20cb7ceef47
Dns2690 <107601365+Dns2690@users.noreply.github.com>
2022-08-25 19:45:34 -0600

    Update IniciarSesionController.js
    validacion de estado

--- public/js/controllers/IniciarSesionController.js
+++ public/js/controllers/IniciarSesionController.js
@@ -2,8 +2,20 @@
 let btnIniciar = document.getElementById("btnIniciar");
 let inputUsuarioLogin = document.getElementById("txtUsuario-l");
 let inputContrasenha1Login = document.getElementById("txtContrasenha-l");
-
+let listaUsuarios;
 btnIniciar.addEventListener("click", IniciarSesion);
+
+window.addEventListener("load", () => {
+    GetlistaUsuarios();
+  });
+
+async function GetlistaUsuarios() {
+    let result = await getUsuariosArray();
+    if (result != {} && result.resultado == true) {
+      listaUsuarios = result.ListaUsuariosBD;
+      console.log(listaUsuarios)
+    }
+  }
 
 async function IniciarSesion() {
     if (await ValidarDatosLogin()) {
@@ -11,9 +23,17 @@ async function IniciarSesion() {
         Email = Email.trim();
         let result = await validarLogin(Email, inputContrasenha1Login.value);
         if (result) {
-            ConfirmarDatos("¡Sesión iniciada!");
-            location.href = './AppVerCitas.html'
-        } else {
+            for (let i = 0; i < listaUsuarios.length; i++){
+                if(Email == listaUsuarios[i].Email){
+                    if (listaUsuarios[i].Activo !=1){
+                        MostrarError("¡Usuario inactivo, contacte al administrador!");
+                    }else{
+                        ConfirmarDatos("¡Sesión iniciada!");
+                        location.href = './AppVerCitas.html'
+                    }
+                }
+            }
+        }else {
             MostrarError("¡Usuario o contraseña incorrectos!");
         }
     }
@@ -38,3 +58,5 @@ async function ValidarDatosLogin() {
     }
     return true;
 }
+
+                    
